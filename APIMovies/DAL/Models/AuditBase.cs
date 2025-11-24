3689bf251d13fb714aa36ab4cf05d6ee8e6bd372
Sebastian Franco <Sebastian Franco@DESKTOP-0E6D6GB>
2025-11-24 17:31:51 -0500

    Add project files.

---
+++ APIMovies/DAL/Models/AuditBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMovies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key]
+        
+        public int Id { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; }
+        
+        public virtual DateTime ModifiedDate { get; set; }
+
+    }
+}
